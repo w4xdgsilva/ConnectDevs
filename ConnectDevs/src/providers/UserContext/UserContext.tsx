20d7fdd6e37dd0b56906b0f43d0c57f3d7b8962a
CreeperEvil1227 <legendarycake1227@gmail.com>
2023-03-08 14:09:26 -0300

    feat: added user context

---
+++ ConnectDevs/src/providers/UserContext/UserContext.tsx
@@ -0,0 +1,133 @@
+import { createContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { AxiosError } from 'axios';
+import { toast } from 'react-toastify';
+import { iUserContext, iDefaultProviderProps, iUserData, iApiUserResponseData, iDefaultErrorResponse, iRegisterFormData, iLoginFormData, iApiAutoLoginResponseData } from './@types';
+import { api } from '../../services/api'
+
+export const UserContext = createContext({} as iUserContext)
+
+export const UserProvider = ({ children }: iDefaultProviderProps) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [userData, setUserData] = useState<iUserData | null>(null)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const userToken = JSON.parse(localStorage.getItem('@CONNECTDEVS:TOKEN') || 'null')
+    const userId = JSON.parse(localStorage.getItem('@CONNECTDEVS:USER') || 'null')
+
+    if (userToken) {
+      const autoLogin = async () => {
+        try {
+          const response = await api.get<iApiAutoLoginResponseData>(`/users/${userId}`, {
+            headers: {
+              Authorization: `Bearer ${userToken}`
+            }
+          })
+          
+          setUserData({
+            id: response.data.id,
+            name: response.data.name,
+            email: response.data.email,
+            username: response.data.username
+          })
+
+          navigate('/')
+
+        } catch (error) {
+          localStorage.removeItem('@CONNECTDEVS:TOKEN')
+          localStorage.removeItem('@CONNECTDEVS:USER')
+          setUserData(null)
+        }
+      }
+
+      autoLogin()
+    }
+  }, [])
+
+  const userRegister = async (formData: iRegisterFormData) => {
+    try {
+      const response = await api.post<iApiUserResponseData>('/register', formData)
+      setIsLoading(true)
+      const userId = parseInt(response.data.user.id, 10)
+
+      setUserData({
+        id: userId,
+        name: response.data.user.name,
+        email: response.data.user.email,
+        username: response.data.user.username
+      })
+
+      localStorage.setItem('@CONNECTDEVS:USER', JSON.stringify(response.data.user.id))
+      localStorage.setItem('@CONNECTDEVS:TOKEN', JSON.stringify(response.data.accessToken))
+      navigate('/login')
+      toast.success('Cadastro realizado com sucesso!')
+
+    } catch (error) {
+      const currentError = error as AxiosError<iDefaultErrorResponse>
+
+      const errorMessage = currentError.response?.data
+
+      if (typeof errorMessage === 'string' && errorMessage === 'Email already exists') {
+        toast.error('Email já cadastrado')
+      }
+
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const userLogin = async (formData: iLoginFormData) => {
+    try {
+      const response = await api.post<iApiUserResponseData>('/login', formData)
+      setIsLoading(true)
+      const userId = parseInt(response.data.user.id, 10)
+
+      setUserData({
+        id: userId,
+        name: response.data.user.name,
+        email: response.data.user.email,
+        username: response.data.user.username
+      })
+
+      localStorage.setItem('@CONNECTDEVS:USER', JSON.stringify(response.data.user.id))
+      localStorage.setItem('@CONNECTDEVS:TOKEN', JSON.stringify(response.data.accessToken))
+      navigate('/')
+      toast.success('Login realizado com sucesso!')
+
+    } catch (error) {
+      const currentError = error as AxiosError<iDefaultErrorResponse>
+
+      const errorMessage = currentError.response?.data
+
+      if (typeof errorMessage === 'string' && errorMessage === 'Password is too short') {
+        toast.error('A senha deve conter pelo menos oito caracteres')
+
+      } else if (typeof errorMessage === 'string' && errorMessage === 'Cannot find user') {
+        toast.error('Email ou senha incorretos')
+      }
+
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const userLogout = () => {
+    setUserData(null)
+    localStorage.removeItem('@CONNECTDEVS:TOKEN')
+    navigate('/login')
+  }
+
+  return (
+    <UserContext.Provider value={{
+      isLoading,
+      userRegister,
+      userLogin,
+      userLogout,
+      userData
+    }}>
+      {children}
+    </UserContext.Provider>
+  )
+};
