e38e8cd9989cc41cd6d0d46ed3b26c463ceb42c9
CreeperEvil1227 <legendarycake1227@gmail.com>
2023-03-08 14:07:35 -0300

    feat: added login page component

--- ConnectDevs/src/pages/LoginPage/index.tsx
+++ ConnectDevs/src/pages/LoginPage/index.tsx
@@ -1,3 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { LoginForm } from '../../components/Forms/Login';
 
-export const LoginPage = () => <div>index</div>;
+export const LoginPage = () => (
+    <main>
+        <section>
+            <h1>{'<ConnectDEVs/>'}</h1>
+            <h2>Login</h2>
+            <p>Entre na maior rede de developers do brasil</p>
+            <LoginForm />
+            <Link to='/register'>Ainda não tem uma conta? Cadastre-se aqui</Link>
+        </section>
+    </main>
+)
