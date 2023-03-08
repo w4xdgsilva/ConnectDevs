73ed7bdf2a7c394cabf74a5a63b7d77ca15c9d2c
CreeperEvil1227 <legendarycake1227@gmail.com>
2023-03-08 18:21:47 -0300

    style: added theme provider

--- ConnectDevs/src/main.tsx
+++ ConnectDevs/src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 import Providers from './providers/providers';
+import { mainTheme } from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Providers>
-        <App />
+        <ThemeProvider theme={mainTheme}>
+          <App />
+        </ThemeProvider>
       </Providers>
     </BrowserRouter>
   </React.StrictMode>
