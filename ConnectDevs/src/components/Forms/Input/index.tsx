4361851d2cba21d8df554a953ccbd5adc28afb60
CreeperEvil1227 <legendarycake1227@gmail.com>
2023-03-08 12:53:56 -0300

    feat: created input component

--- ConnectDevs/src/components/Forms/Input/index.tsx
+++ ConnectDevs/src/components/Forms/Input/index.tsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
+import { MdVisibility, MdVisibilityOff } from "react-icons/md"
+import { TextField } from "@mui/material"
 
-export const index = () => {
-  return <div>index</div>;
+interface iInputProps {
+  label?: string
+  type: 'text' | 'email' | 'password'
+  placeholder: string
+  register: UseFormRegisterReturn<string>
+  hiddenButton: boolean
+  error?: FieldError
+}
+
+export const Input = ({ label, type, placeholder, register, hiddenButton, error }: iInputProps) => {
+  const [isHidden, setIsHidden] = useState(true)
+
+  return (
+    <fieldset>
+      <TextField
+        label={label || ''}
+        type={type === 'password' && isHidden ? 'password' : 'text'}
+        placeholder={placeholder}
+        {...register}
+      />
+
+      {hiddenButton ? (
+        <button type='button' onClick={() => setIsHidden(!isHidden)}>
+          {isHidden ? <MdVisibility /> : <MdVisibilityOff />}
+        </button>
+      ) : null}
+      
+      {error ? <p>
+        {error.message}
+      </p> : null}
+    </fieldset>
+  )
 };
