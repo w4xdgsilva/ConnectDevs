8fe82a970bddb5d4c9a61f050ba0a671c703b868
CreeperEvil1227 <legendarycake1227@gmail.com>
2023-03-08 11:41:53 -0300

    feat: added providers import

---
+++ ConnectDevs/src/providers/providers.tsx
@@ -0,0 +1,10 @@
+import { iDefaultProviderProps } from './UserContext/@types';
+import { UserProvider } from './UserContext/UserContext';
+
+const Providers = ({ children }: iDefaultProviderProps) => (
+    <UserProvider>
+        {children}
+    </UserProvider>
+)
+
+export default Providers
