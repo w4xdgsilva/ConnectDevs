6ee34978ab97eb924d0107a28d972a2126d0adba
CreeperEvil1227 <legendarycake1227@gmail.com>
2023-03-08 13:44:59 -0300

    feat: envolved home page route with protected route

--- ConnectDevs/src/routes.tsx
+++ ConnectDevs/src/routes.tsx
@@ -5,10 +5,13 @@ import { HomePage } from './pages/HomePage/index';
 import { LoginPage } from './pages/LoginPage/index';
 import { RegisterPage } from './pages/RegisterPage/index';
 import { ProfilePage } from './pages/ProfilePage/index';
+import { ProtectedRoutes } from './components/ProtectedRoutes/ProtectedRoutes';
 
 export const AppRouter = () => (
   <Routes>
-    <Route path='/' element={<HomePage />} />
+    <Route path='/' element={<ProtectedRoutes />}>
+      <Route index element={<HomePage />} />
+    </Route>
     <Route path='/login' element={<LoginPage />} />
     <Route path='/register' element={<RegisterPage />} />
     <Route path='/profilePage' element={<ProfilePage />} />
