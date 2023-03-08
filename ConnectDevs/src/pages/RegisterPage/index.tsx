b51046a9cb68f051512c711fbd2ec7c0747736af
CreeperEvil1227 <legendarycake1227@gmail.com>
2023-03-08 14:06:58 -0300

    feat: added register page component

--- ConnectDevs/src/pages/RegisterPage/index.tsx
+++ ConnectDevs/src/pages/RegisterPage/index.tsx
@@ -1,3 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { RegisterForm } from '../../components/Forms/Register';
 
-export const RegisterPage = () => <div>index</div>;
+export const RegisterPage = () => (
+    <main>
+        <section>
+            <h1>{'<ConnectDEVs/>'}</h1>
+            <h2>Cadastro</h2>
+            <p>Embarque na maior rede de developers do brasil</p>
+            <RegisterForm />
+            <Link to='/login'>Já tem uma conta? Retornar para o login</Link>
+        </section>
+    </main>
+)
