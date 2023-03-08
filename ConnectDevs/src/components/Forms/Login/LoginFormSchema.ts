97427a1fcd0f6bdcba07678545d5b2a55f4e1ee2
CreeperEvil1227 <legendarycake1227@gmail.com>
2023-03-08 14:08:34 -0300

    feat: added login form schema

---
+++ ConnectDevs/src/components/Forms/Login/LoginFormSchema.ts
@@ -0,0 +1,7 @@
+import * as yup from "yup"
+
+export const loginFormSchema = yup.object().shape({
+    email: yup.string().required("E-mail obrigatório")
+    .email("E-mail inválido"),
+    password: yup.string().required("Senha obrigatória")
+})
