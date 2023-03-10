dc88b908b20e5bd714d561d8c288a2c232f4a041
CreeperEvil1227 <legendarycake1227@gmail.com>
2023-03-10 16:35:07 -0300

    style: changed button componentization and added button style

--- ConnectDevs/src/components/Header/index.tsx
+++ ConnectDevs/src/components/Header/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { StyledButton } from '../../styles/button';
 
 export const Header = () => (
   <header>
     <nav>
       <h1>{'<ConnectDEVs/>'}</h1>
       <p>Pic</p>
-      <button type='button' title='Finalizar sessão'>
+      <StyledButton
+        $buttonSize='medium'
+        $buttonStyle='gray'
+        type='button'
+        title='Finalizar sessão'
+        aria-label='Finalizar sessão'
+      >
         Sair
-      </button>
+      </StyledButton>
     </nav>
   </header>
 );
