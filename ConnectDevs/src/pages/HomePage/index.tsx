aea95234a0ef5ce4d1da81e5e3ca88b67028de05
João Vitor <14vitorjo@gmail.com>
2023-03-12 19:35:09 -0300

    feat: renderLinks homepage

--- ConnectDevs/src/pages/HomePage/index.tsx
+++ ConnectDevs/src/pages/HomePage/index.tsx
@@ -3,11 +3,16 @@ import { Header } from '../../components/Header';
 import { PostsProvider } from '../../providers/PostsContext/PostsContext';
 import { PostsList } from '../../components/PostList/index';
 import { CreatePostForm } from '../../components/Forms/CreatePost';
+import { LinksList } from '../../components/LinksList';
+import { ProfileProvider } from '../../providers/ProfileContext/ProfileContext';
 
 export const HomePage = () => (
+  <ProfileProvider>
   <PostsProvider>
     <Header />
     <CreatePostForm />
     <PostsList />
+    <LinksList/>
   </PostsProvider>
+  </ProfileProvider>
 );
